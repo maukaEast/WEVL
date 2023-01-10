91feca4bcdb83dac3b16b31ab117d2793ab5a811
maukaeast@gmail.com <maukaeast@gmail.com>
2023-01-09 20:14:06 -1000

    Added CSV import functionality. Need to figure out comparison and output of needed list. Also, need to better structure into descrete episodes -possibly just: MainList -> select show date -> output

--- first_church.py
+++ first_church.py
@@ -1,5 +1,11 @@
 '''
 Playlist scraper for WEVL's First Church of Rock
+
+https://stackoverflow.com/questions/71545135/how-to-append-rows-with-concat-to-a-pandas-dataframe
+https://datascientyst.com/compare-two-pandas-dataframes-get-differences/
+https://stackoverflow.com/questions/20225110/comparing-two-dataframes-and-getting-the-differences
+https://www.geeksforgeeks.org/how-to-compare-two-dataframes-with-pandas-compare/
+
 '''
 import requests
 import pandas as pd
@@ -7,14 +13,20 @@ import numpy as np
 
 from bs4 import BeautifulSoup
 
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
 #- - setup variables and global uses
 episode_list = []
 global DBase
+global Indielist
+
 
 def page_search(inp_url):
 	global quote_list
 	global index
 	global DBase
+	global Indielist
 	print("---- ----- -----")
 		
 	site_inp=inp_url
@@ -25,18 +37,16 @@ def page_search(inp_url):
 		thelink = str('https://spinitron.com/'+link.find(class_='link').get('href'))
 		episode_details(thelink)	
 		#episode_list.append(each)
-	
-	epURL = 'https://spinitron.com/WEVL/pl/16757704/The-1st-Church-of-Rock'
+
 #	episode_details(epURL)	
 		
 def episode_details(inp_url):
 	global DBase
-	#this is to make up for eventual retirement of 'append' in pandas
-	tmp = []
+	global Indielist
+	#print(inp_url)
 	
-	print(inp_url)
 	show_details = requests.get(inp_url)
-	showsoup =  BeautifulSoup(show_details.content.decode('utf-8'), "lxml")
+	showsoup =	BeautifulSoup(show_details.content.decode('utf-8'), "lxml")
 	showdate = (showsoup.find('p', class_='timeslot').text).lstrip()[:12]
 	print(showdate)
 	listings = showsoup.find_all('div', class_='spins public-spins')
@@ -52,30 +62,40 @@ def episode_details(inp_url):
 				date = tracks.find(class_='released').text
 				listing = (artist+' - '+song+' - '+album+' - '+date)
 				dict = {"show_date":showdate,
-						"artist":artist,
-						"song":song,
-						"album":album,
-						"date":date				
+						"Artist":artist,
+						"Title":song,
+						"Album":album,
+						"Year":date				
 					}
-				tmp.append(dict, ignore_index = True)
+				DBase = pd.concat([DBase, pd.DataFrame([dict])])
 			except:
 				pass
-			try:
-				print (listing)
-			except:
-				pass
-	print (" -- -- ")
-	DBase = pd.concat(DBase, tmp)
+			#try:
+				#print (listing)
+			#except:
+			#	pass
+	#print (" -- -- ")
+	#DBase = pd.concat([DBase, pd.DataFrame([tmp])])
 	#print(DBase)
-'''  
+'''	 
   with open('bored.txt','w') as my_list_file:
 		file_content = '\n'.join(episode_list)
 		my_list_file.write(file_content)
 '''
 def panda_make():
 	global DBase
-	headings = ["show_date","artist","song","album","date"]
+	global Indielist
+	
+	headings = ["show_date","Artist","Title","Album","Year"]
 	DBase = pd.DataFrame(columns=headings)
+	format_monkey()
+
+def format_monkey():
+	global Indielist
+	
+	Indielist = pd.read_csv("indieplaylist.csv")
+	Indielist = Indielist.drop(['Rating','Bitrate','Genre','Length','Path','Media'],axis=1)
+	print(Indielist)
 
 # MAIN FUNCTION
 panda_make()
@@ -92,7 +112,6 @@ while page !=2:
 	url = prefix+count
 	page = page+1
 	page_search(url)
-print(DBase)
 print("scrape complete")
 
 '''
